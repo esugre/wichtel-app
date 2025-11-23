036b246be8eddf6155adf3c9c4909b41369f8401
Rafael <Rafael@users.noreply.gthub.com>
2025-11-23 10:29:42 +0100

    Add Firebase Integration

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOsE04GG63WyH-OwEjLIk3xexWO5Jz6T8",
+  authDomain: "wichtelingo.firebaseapp.com",
+  projectId: "wichtelingo",
+  storageBucket: "wichtelingo.firebasestorage.app",
+  messagingSenderId: "140074215720",
+  appId: "1:140074215720:web:100b80aa3891b8c51b081d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
